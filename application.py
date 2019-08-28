140972b5671ebf6ad3090a7858909b91b37909a8
Dhruv Upadhyay <dhruv.hbk8@gmail.com>
2019-08-28 19:09:04 +0530

    Add error page

--- application.py
+++ application.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-import logging
 import models
 application = Flask(__name__)
 application.config.from_object('config')
 application.debug = True
 db = SQLAlchemy(application)
 
+
+# admin credentials
+admin_email = "dhruv@gmail.com"
+admin_pass = "123"
 
 @application.route("/")
 def main():
@@ -32,6 +35,7 @@ def get_records():
     application.logger.info(records)
     return render_template("records.html", records=records)
 
+
 @application.route("/clear", methods=['GET','POST'])
 def clear_db():
     # application.logger.info()
@@ -39,6 +43,22 @@ def clear_db():
     number =db.session.query(models.AttendanceRecord).delete()
     db.session.commit()
     return redirect('/getrecords')
+
+
+@application.route("/admin", methods=['GET','POST'])
+def show_admin():
+    return render_template('admin.html')
+
+
+@application.route("/validate", methods=['POST'])
+def validate():
+    email = request.form['email']
+    password = request.form['pass']
+
+    if email == admin_email and admin_pass == password:
+        return redirect('/getrecords')
+    else:
+        return render_template('error.html')
 
 
 if __name__ == "__main__":
